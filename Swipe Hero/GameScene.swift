5a214c7cbfff3cb6712cc0c34bb1238db7418a38
Andre Sakiyama <andresakiyama@Andres-MacBook-Pro-5.local>
2015-06-17 10:14:40 -0300

    Swipe Recognizer

--- Swipe Hero/GameScene.swift
+++ Swipe Hero/GameScene.swift
@@ -18,6 +18,75 @@ class GameScene: SKScene {
 //        
 //        self.addChild(myLabel)
         
+        /*Right and Left Views*/
+        var leftView : UIView = UIView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width/2, UIScreen.mainScreen().bounds.size.height))
+        leftView.backgroundColor = UIColor.clearColor()
+        
+        var rightView = UIView(frame: CGRectMake(UIScreen.mainScreen().bounds.size.width/2, 0, UIScreen.mainScreen().bounds.size.width/2, UIScreen.mainScreen().bounds.size.height))
+        rightView.backgroundColor = UIColor.clearColor()
+        
+        self.view?.addSubview(leftView)
+        self.view?.addSubview(rightView)
+        
+        
+        /*Gesture recognizer left view*/
+        let swipeRightLeftViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeRightLeftView:")
+        let swipeLeftLeftViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeLeftLeftView:")
+        let swipeUpLeftViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeUpLeftView:")
+        let swipeDownLeftViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeDownLeftView:")
+        
+        swipeRightLeftViewRecognizer.direction = .Right
+        swipeLeftLeftViewRecognizer.direction = .Left
+        swipeUpLeftViewRecognizer.direction = .Up
+        swipeDownLeftViewRecognizer.direction = .Down
+        
+        leftView.addGestureRecognizer(swipeRightLeftViewRecognizer)
+        leftView.addGestureRecognizer(swipeLeftLeftViewRecognizer)
+        leftView.addGestureRecognizer(swipeUpLeftViewRecognizer)
+        leftView.addGestureRecognizer(swipeDownLeftViewRecognizer)
+        
+        /*Gesture Recognizer right view*/
+        let swipeRightRightViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeRightRightView:")
+        let swipeLeftRightViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeLeftRightView:")
+        let swipeUpRightViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeUpRightView:")
+        let swipeDownRightViewRecognizer: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action:"swipeDownRightView:")
+        
+        swipeRightRightViewRecognizer.direction = .Right
+        swipeLeftRightViewRecognizer.direction = .Left
+        swipeUpRightViewRecognizer.direction = .Up
+        swipeDownRightViewRecognizer.direction = .Down
+        
+        rightView.addGestureRecognizer(swipeRightRightViewRecognizer)
+        rightView.addGestureRecognizer(swipeLeftRightViewRecognizer)
+        rightView.addGestureRecognizer(swipeUpRightViewRecognizer)
+        rightView.addGestureRecognizer(swipeDownRightViewRecognizer)
+        
+    }
+    
+    func swipeRightLeftView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Right Left View")
+    }
+    func swipeLeftLeftView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Left Left View")
+    }
+    func swipeUpLeftView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Up Left View")
+    }
+    func swipeDownLeftView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Down Left View")
+    }
+    
+    func swipeRightRightView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Right Right View")
+    }
+    func swipeLeftRightView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Left Right View")
+    }
+    func swipeUpRightView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Up Right View")
+    }
+    func swipeDownRightView(swipe:UISwipeGestureRecognizer) {
+        println("Swipe Down Right View")
     }
     
     override func update(currentTime: CFTimeInterval) {
