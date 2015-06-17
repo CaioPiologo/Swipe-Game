ac914021bf48fe1b15b85d09b29c17426fa23f00
Caio Vinícius Piologo Véras Fernandes <caiovpiologo@gmail.com>
2015-06-17 09:11:05 -0300

    With an arrow

---
+++ Swipe Hero/Arrow.swift
@@ -0,0 +1,27 @@
+//
+//  Arrow.swift
+//  Swipe Hero
+//
+//  Created by Caio Vinícius Piologo Véras Fernandes on 6/17/15.
+//  Copyright (c) 2015 Caio. All rights reserved.
+//
+
+import SpriteKit
+
+enum Direction:Int {
+    case UP = 0, RIGHT, DOWN, LEFT
+}
+
+class Arrow:SKNode {
+    let direction:Direction
+    
+    init(direction:Direction) {
+        self.direction = direction
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
